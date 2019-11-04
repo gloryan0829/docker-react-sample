89074e1164fb98eb7b1138aad6dfce8aaa0d5725
이정주 <jj.lee@daou.co.kr>
2019-11-04 22:42:51 +0900

    change beanstalk spec from t2.micro to t2.small

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>src/App.js</code> and save to reload.
+          <code>자동 배포하기 : src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
