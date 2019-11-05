90d3c798d0820010db38e811ca079b97de9d306e
이정주 <jj.lee@daou.co.kr>
2019-11-05 10:18:21 +0900

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>자동 배포하기 : src/App.js</code> and save to reload.
+           자동 배포하기 : feature/ui/ui-text-변경 브랜치 -> PR -> Merge.
         </p>
         <a
           className="App-link"
