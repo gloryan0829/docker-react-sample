9963e6af54941007eac83eab5340a4028decd111
이정주 <jj.lee@daou.co.kr>
2019-11-06 10:53:50 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-           자동 배포하기 : feature/ui/ui-text-변경 브랜치 -> PR -> Merge!!!(Modified)
+           배포하기 : feature/ui/ui-text-변경 브랜치 -> PR -> Merge!!!(Modified)
         </p>
         <a
           className="App-link"
