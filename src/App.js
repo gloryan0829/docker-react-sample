282ce733ca36ccebe9cc4379eb601ec9e4c308af
이정주 <jj.lee@daou.co.kr>
2019-11-06 10:55:40 +0900

    변경-#6

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>자동 배포하기 : src/App.js</code> and save to reload.
+          <code>자동 배포하기 : src/App.js</code> and save to reload. (수정)
         </p>
         <a
           className="App-link"
